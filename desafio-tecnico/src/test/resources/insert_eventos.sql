0f7cb7d5c1181f380981080ea3ea28c74a1356cf
JamilZerati <76897006+JamilZerati@users.noreply.github.com>
2024-10-28 08:11:38 -0300

    Delete insert_eventos não utilizado

--- desafio-tecnico/src/test/resources/insert_eventos.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO instituicoes (id, nome, tipo) VALUES (1, 'Instituicao 1', 'TIPO1');
-
-INSERT INTO eventos (evento_id, nome, data_inicial, data_final, ativo, instituicao_id )
-VALUES (1, 'Evento 1', '2024-10-01', '2024-10-30',true, 1);
-
-INSERT INTO eventos (evento_id, nome, data_inicial, data_final, ativo, instituicao_id )
-VALUES (2, 'Evento 2', '2024-10-01', '2024-10-26',false, 1);
